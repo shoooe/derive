8c283a9d30c65137f362f096a70894dd613b882c
Shoe <tpigarelli@gmail.com>
2022-10-19 12:42:10 +0200

    -

---
+++ test/Alias.test.ts
@@ -0,0 +1,15 @@
+import { assertEqualTypes } from '../utils/assertEqualTypes';
+import { assertNonEqualTypes } from '../utils/assertNonEqualTypes';
+import { test } from '../utils/test';
+import { Alias } from '../src/Alias';
+import { Auto } from '../src/Auto';
+
+type User = {
+  id: number;
+  name: string | null;
+};
+
+test('Alias', [
+  assertNonEqualTypes<Alias<User, 'id', Auto>, Alias<User, 'name', Auto>>(),
+  assertEqualTypes<Alias<User, 'name', Auto>, Alias<User, 'name', Auto>>(),
+]);
---
+++ utils/assertNonEqualTypes.ts
@@ -0,0 +1,11 @@
+import { assertEqualTypes } from './assertEqualTypes';
+
+/**
+ * Asserts that two types are not exactly the same.
+ *
+ * @see {@link https://github.com/millsp/ts-toolbelt/blob/7d6c44df57c5024d565041e33894660d868a2d86/sources/Any/Equals.ts}
+ */
+export declare function assertNonEqualTypes<
+  Type,
+  Expect,
+>(): typeof assertEqualTypes<Type, Expect> extends true ? false : true;
